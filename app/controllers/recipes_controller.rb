f17b753822933eced0268679dcee745a9fd05da5
Jeff Shearer <jeff.r.shearer@gmail.com>
2016-02-09 14:02:53 -0800

    added form validation for recipes, batch links on recipes index

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -69,6 +69,6 @@ class RecipesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def recipe_params
-      params.require(:recipe).permit(:name,:description,:style)
+      params.require(:recipe).permit(:name,:description,:style,:og,:fg,:abv,:srm,:ibu)
     end
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,4 +1,4 @@
 class Recipe < ActiveRecord::Base
 	has_many :batches
-	has_many :ingredients
+	has_many :ingredients, dependent: :destroy
 end
