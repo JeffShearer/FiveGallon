cb97b6fdaa6cfbd5ae63ae2efea2ea214a324234
Jeff <jeff.r.shearer@gmail.com>
2016-02-13 12:23:11 -0800

    bootstrap customizations and created sample batch page mockup

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get 'pages/about'
 
+  get 'batches/sample'
+
   get 'pages/contact'
 
   # The priority is based upon order of creation: first created -> highest priority.
